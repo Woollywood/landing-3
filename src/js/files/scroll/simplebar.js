e4f68669f669300b55d5fe0c483dd986db13aeba
Woollywood <Gerbert.Wells969@yandex.ru>
2023-06-07 01:50:02 +0300

    init

---
+++ src/js/files/scroll/simplebar.js
@@ -0,0 +1,16 @@
+// Подключение плагина из node_modules
+import SimpleBar from "simplebar";
+// Подключение стилей из node_modules
+import "simplebar/dist/simplebar.css";
+
+// Добавляем в блок атрибут data-simplebar
+// Также можно инициализировать следующим кодом, используя настройки
+/*
+if (document.querySelectorAll('[data-simplebar]').length) {
+	document.querySelectorAll('[data-simplebar]').forEach(scrollBlock => {
+		new SimpleBar(scrollBlock, {
+			autoHide: false
+		});
+	});
+}
+*/
