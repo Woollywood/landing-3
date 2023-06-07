d843789bd0a788c96112d5e4c5d15f92630bec08
Woollywood <Gerbert.Wells969@yandex.ru>
2023-06-07 14:31:38 +0300

    v1.0

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,7 +5,22 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 window.onload = () => {
-	document.addEventListener("click", (e) => {
+
+	// Если расширение экрана при изменении их мобильной версии
+	// переходит в планшетную, убираем мобильные модификаторы классов
+	window.addEventListener('resize', e => {
+		if (e.target.innerWidth > 767.98) {
+			document.body.classList.remove('menu-show');
+		}
+	});
+
+	// Клик по меню-бургеру
+	document.addEventListener("click", e => {
 		const targetElement = e.target;
+
+		if (targetElement.closest('.menu__icon')) {
+			document.body.classList.toggle('menu-show');
+			document.body.classList.toggle('overflow-hidden');;
+		}
 	});
 };
